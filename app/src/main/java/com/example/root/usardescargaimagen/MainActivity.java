f9fe0b644ccf8b91e5456850aa8cb36226ec20ea
Rene Soto <rene.soto01@iest.edu.mx>
2015-04-28 08:06:40 -0500

    Descargar y guardar imagen en android.

---
+++ app/src/main/java/com/example/root/usardescargaimagen/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.root.usardescargaimagen;
+
+/**
+ * Created by root on 27/04/15.
+ */
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+    Button load_img,save_img;
+    ImageView img;
+    Bitmap bitmap;
+    ProgressDialog pDialog;
+    String image = "naruhina.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        load_img = (Button)findViewById(R.id.load);
+        save_img = (Button)findViewById(R.id.save);
+        img = (ImageView)findViewById(R.id.img);
+        load_img.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                new LoadImage().execute("https://mmi229.whatsapp.net/d/3C7dEQzYP2WmzM4U-XAUS1U-bXo/AqQsXevYj4L5JBY2ZOc6sx1Lz5NqsWW7lMaLqHAziY6u.jpg");
+            }
+        });
+
+        save_img.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                guardar_imagen();
+                }
+        });
+
+
+    }
+
+    public void guardar_imagen() {
+        if (bitmap != null) {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File path = new File(sdCard.getAbsolutePath() + "/myImages");
+            if (!path.exists())
+                path.mkdirs();
+            File file = new File(path, image);
+            FileOutputStream fOut;
+            try {
+                fOut = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+                fOut.flush();
+                fOut.close();
+                Toast.makeText(MainActivity.this, "Imagen guardada", Toast.LENGTH_LONG)
+                        .show();
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "No se ha bajado la imagen",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Loading Image ....");
+            pDialog.show();
+
+        }
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap image) {
+
+            if(image != null){
+                img.setImageBitmap(image);
+                pDialog.dismiss();
+
+            }else{
+
+                pDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+}
